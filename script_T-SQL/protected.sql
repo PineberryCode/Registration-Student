71b45e212a78ff0a98ddbe3e9c5eb8e1161cad4d
MINDLUNNY <88601032+PineberryCode@users.noreply.github.com>
2023-04-23 17:02:23 -0500

    Update protected.sql

--- script_T-SQL/protected.sql
+++ script_T-SQL/protected.sql
@@ -1,10 +1,10 @@
 create database protected;
 use protected;
 create table student (
-	dni 		int,
-    	cod_student 	bigint 		not null,
-    	num_orden 	int 		not null,
-	names 		varchar(100) 	not null,
-	lastname 	varchar(100) 	not null,
+	dni		int,
+    	cod_student	bigint		not null,
+    	num_orden	int 		not null,
+	names		varchar(100) 	not null,
+	lastname	varchar(100) 	not null,
 	primary key (dni)
 );
